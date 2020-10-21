de90089343bb2cd52ef7dc720fc183f5964b9cc5
GeronimoJohn <johnageronimo1@gmail.com>
2020-10-22 11:53:30 +1300

    Images and SCSS files added

---
+++ app.js
@@ -0,0 +1,6 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "77f867d92inh",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "AhTMGjIGx1nflRAs-lxRR0bcD-osLEk-DQqKeIKLqJs"
+});
